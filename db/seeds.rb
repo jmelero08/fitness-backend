41ca1c83a22f0dc98ec2a25d5daf104956446a52
Jose Melero <jmelero08@gmail.com>
2021-10-22 12:34:08 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Workout.destroy_all
+
+User.create(name: "Jose", username: "jmelero", password: "123")
+Workout.create(name: "Chest Day", description: "Bench Press, Incline Bench", user_id: 1)
